13627502138c2a5c895f1556feffaa95d9350f1c
阳葵 <yaakua@126.com>
2018-09-07 11:33:02 +0800

    解决打包问题

---
+++ ueboot-view/examples/main.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/router'
+import ueboot from '../src/index'
+import {Log, Axios} from '../src/index'
+import iView from 'iview';
+/*自定义样式*/
+import '../src/styles/index.less';
+
+// import "font-awesome/css/font-awesome.min.css"
+
+Vue.use(iView);
+Vue.use(ueboot);
+Log.config({level:3})
+ueboot.Axios.init({baseURL: "", unauthorizedUrl: '/#/login'})
+
+
+//Log.config({"level":0});
+//设置加载提示
+/*router.beforeEach((transition) => {
+    iView.LoadingBar.start();
+});
+
+router.afterEach((transition) => {
+    iView.LoadingBar.finish();
+});*/
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    template: '<div><App></App></div>',
+    components: { App }
+})
