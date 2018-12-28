def5534d688023e6607456cee2c23424709099ca
阳葵 <yaakua@126.com>
2018-12-28 15:38:02 +0800

    更新版本到2.2.1，解决代码生成器无法运行的问题

--- ueboot-shiro/src/test/java/com/ueboot/shiro/util/PasswordUtilTest.java
+++ ueboot-shiro/src/test/java/com/ueboot/shiro/util/PasswordUtilTest.java
@@ -7,6 +7,11 @@ public class PasswordUtilTest {
 
    @Test
     public void sha512() {
-       Assert.assertEquals("d02db260d58a26d777bfa1c8dd41b2a0492024e64cd69c3401223dcea60c8b3d2a44276748e0de9e763b630e84ef1a362b8ed370a9849a2346df769a25c3f8f3",PasswordUtil.sha512("admin","111111"));
+       Assert.assertEquals("d02db260d58a26d777bfa1c8dd41b2a0492024e64cd69c3401223dcea60c8b3d2a44276748e0de9e763b630e84ef1a362b8ed370a9849a2346df769a25c3f8f3",PasswordUtil.sha512("zhenguo","111111"));
+    }
+
+    @Test
+    public void password(){
+        System.out.println(PasswordUtil.sha512("zhenguo","111111"));
     }
 }
