be20021892570f82a1545880dbbdcb1c2505e422
Yongfeng Hou <yongfeng.hou@gtb.com>
2019-06-06 19:16:55 +0800

    single——>get

--- ueboot-core/src/main/java/com/ueboot/core/controller/BasicController.java
+++ ueboot-core/src/main/java/com/ueboot/core/controller/BasicController.java
@@ -10,8 +10,8 @@ public class BasicController<T> {
     protected static final String UPDATE_CH = "修改";
     protected static final String DELETE = "delete";
     protected static final String DELETE_CH = "删除";
-    protected static final String SINGLE = "single";
-    protected static final String SINGLE_CH = "查询单条";
+    protected static final String GET = "get";
+    protected static final String GET_CH = "查询单条";
     protected static final String LIST = "list";
     protected static final String LIST_CH = "列表查询";
     protected static final String PAGE = "page";
@@ -22,7 +22,5 @@ public class BasicController<T> {
     protected static final int LOG_SELECT = 4;
     protected static final String AUTH_YES = "鉴权: 是";
     protected static final String AUTH_NO = "鉴权: 否";
-    protected static final String POST = "POST";
-    protected static final String GET = "GET";
     protected final ConverterDtv<T> converterDTV = new ConverterDtv<>();
 }
