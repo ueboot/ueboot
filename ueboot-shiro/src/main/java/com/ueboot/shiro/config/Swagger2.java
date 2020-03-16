431473111c001d6559bf1884dd7868a49a43b0b2
阳葵 <yaakua@126.com>
2020-03-16 16:53:50 +0800

    对shiro当中的swagger增加可配置方式

--- ueboot-shiro/src/main/java/com/ueboot/shiro/config/Swagger2.java
+++ ueboot-shiro/src/main/java/com/ueboot/shiro/config/Swagger2.java
@@ -1,5 +1,6 @@
 package com.ueboot.shiro.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -8,7 +9,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * swagger2相关配置
@@ -16,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @author yangkui
  */
 @Configuration
+@ConditionalOnExpression("'${ueboot.shiro.swagger.enable}'")
 public class Swagger2 {
     @Bean
     public Docket createRestApi() {
