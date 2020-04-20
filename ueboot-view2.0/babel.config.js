43e30af71f9230cf2a1ebfb8a66ab4c44ca99367
阳葵 <yaakua@126.com>
2020-04-20 17:48:16 +0800

    解决打包问题

--- ueboot-view2.0/babel.config.js
+++ ueboot-view2.0/babel.config.js
@@ -1,9 +1,8 @@
 console.log("#######读取到babel配置文件，开始ES6转ES5#####");
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
-    {
-      useBuiltIns:false
-    }
+    ["@vue/cli-plugin-babel/preset", {
+      "useBuiltIns": false
+    }]
   ]
 }
