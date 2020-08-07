a43af51ec88e98c8e3dc9d38e09abe206186cc4c
阳葵 <kui.yang@xiqiao.io>
2020-08-07 10:06:51 +0800

    解决redis 解析转换问题

--- ueboot-shiro/src/main/java/com/ueboot/shiro/config/Swagger2.java
+++ ueboot-shiro/src/main/java/com/ueboot/shiro/config/Swagger2.java
@@ -3,12 +3,15 @@ package com.ueboot.shiro.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.function.Predicate;
 
 /**
  * swagger2相关配置
@@ -20,10 +23,11 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class Swagger2 {
     @Bean
     public Docket createRestApi() {
+        Predicate<RequestHandler> handlerPredicate =  RequestHandlerSelectors.basePackage("com.ueboot.shiro.controller");
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ueboot.shiro.controller"))
+                .apis(handlerPredicate)
                 .paths(PathSelectors.any())
                 .build();
     }
