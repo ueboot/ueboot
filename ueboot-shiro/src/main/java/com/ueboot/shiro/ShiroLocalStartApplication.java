a7884926798a7d3a6f7c3b3369c0841b8c4e2ffd
阳葵 <kui.yang@xiqiao.io>
2020-08-22 16:05:04 +0800

    解决fastxml版本不一致，导致启动问题

--- ueboot-shiro/src/main/java/com/ueboot/shiro/ShiroLocalStartApplication.java
+++ ueboot-shiro/src/main/java/com/ueboot/shiro/ShiroLocalStartApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 
 /**
@@ -18,7 +17,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 @SpringBootApplication
 @EnableTransactionManagement
 @ComponentScan(value = {"com.ueboot.core","com.ueboot.shiro"})
-@EnableSwagger2WebMvc
 public class ShiroLocalStartApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShiroLocalStartApplication.class, args);
